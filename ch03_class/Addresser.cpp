01b9e2deacb0aa4232d166111458f00e207c74a5
zdsherlock <zd5024@hotmail.com>
2020-08-23 16:42:07 +0800

    commit

---
+++ ch03_class/Addresser.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+	unsigned short shortVar = 5;
+	unsigned long longVar = 65535;
+	long sVar = -65535;
+
+	std::cout << "shortVar:\t" << shortVar;
+	std::cout << "\tAddress of shortVar:\t" << &shortVar << "\n";
+	std::cout << "longVar:\t" << longVar;
+	std::cout << "\tAddress of longVar:\t" << &longVar << "\n";
+	std::cout << "sVar:\t\t" << sVar;
+	std::cout << "\tAddress of sVar:\t" << &sVar << "\n";
+
+	return 0;
+
+}
